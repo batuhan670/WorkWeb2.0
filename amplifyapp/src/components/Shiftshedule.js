ce8cb5c24cec63e369d8472ec51cf0f128792168
Martin <Martin.Widhalm@gmx.at>
2023-01-20 12:28:02 +0100

    Shiftshedulestyle and Code adjustments

--- amplifyapp/src/components/Shiftshedule.js
+++ amplifyapp/src/components/Shiftshedule.js
@@ -6,35 +6,32 @@ import "./ShiftsheduleStyles.css";
 const Weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
 //Arbeitsstunden
-const WorkHoursStart = ["04:00-", "04:00-", "04:00-", "04:00-", "04:00-", "Frei", "Frei"];
+const WorkHoursStart = ["04:00", "04:00", "04:00", "04:00", "04:00", "", ""];
 const WorkHoursEnd = ["12:00", "12:00", "12:00", "12:00", "12:00", "", ""];
 
 //DivElement fuer jeden Tag.
 function getDay(dayName, shiftStart, shiftEnd, number) {
     return <div className="day">
         <div className="dayName">{dayName[number]}</div>
-        <div className="dayHours">{shiftStart[number]}</div>
-        <div className="dayHours">{shiftEnd[number]}</div>
+        <div className="dayHours">
+            <div>{shiftStart[number]}</div>
+            <div>---</div>
+            <div>{shiftEnd[number]}</div>
+        </div>
     </div>
 };
 
 function Shiftshedule() {
-    function changeColor(color) {
-        var r = document.querySelector(':root');
-        r.style.setProperty('--textColor', color);
-    }
-
     return (
         <div id="shiftshedule">
-            <button onClick={() => changeColor("green")}>Green</button>
-            <button onClick={() => changeColor("brown")}>Munggilebru</button>
+            {getDay(Weekdays, WorkHoursStart, WorkHoursEnd, 0)}
+            {getDay(Weekdays, WorkHoursStart, WorkHoursEnd, 1)}
             {getDay(Weekdays, WorkHoursStart, WorkHoursEnd, 2)}
             {getDay(Weekdays, WorkHoursStart, WorkHoursEnd, 3)}
             {getDay(Weekdays, WorkHoursStart, WorkHoursEnd, 4)}
             {getDay(Weekdays, WorkHoursStart, WorkHoursEnd, 5)}
             {getDay(Weekdays, WorkHoursStart, WorkHoursEnd, 6)}
-        </div>
-    );
-};
+        </div>)
+}
 
 export default Shiftshedule;
