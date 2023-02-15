4f8a6d423fc551ee9fc036ba090b1b4a822a4fe0
Martin Widhalm <martin.widhalm@gmx.at>
2023-02-15 02:00:51 +0100

    Made Topline & Logo import comments

--- amplifyapp/src/App.js
+++ amplifyapp/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import './AppStyles.css';
 import './styles/CustomColors.css';
-import Topline from "./components/topline/Topline.js"
-import logo from './logo.svg';
+//import Topline from "./components/topline/Topline.js"
+//import logo from './logo.svg';
 import Overview from "./components/overview/Overview.js"
 import Newsslide from './components/newsslide/Newsslide.js';
 import Shiftshedule from './components/shiftshedule/Shiftshedule.js';
