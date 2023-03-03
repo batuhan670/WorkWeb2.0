28b5e32000f42dcb682077a000942ef94c194c70
Lukas Aichbauer <lukas.aichbauer@wolfvision.net>
2023-03-03 18:03:17 +0100

    - Fixed login path and added constants.

--- amplifyapp/src/logPortal/UserLogin.js
+++ amplifyapp/src/logPortal/UserLogin.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { API_URL } from '../constants';
 import './UserLoginStyles.css'
 
 function Login() {
@@ -8,7 +9,7 @@ function Login() {
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
-            const response = await fetch('http://localhost:3000', {
+            const response = await fetch(API_URL + "/auth", {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
