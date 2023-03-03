646e2b773b8e47ea66b684cef80523097713e3e1
Lukas Aichbauer <lukas.aichbauer@wolfvision.net>
2023-03-03 17:14:04 +0100

    - Added useSelector in DoctorForm.

--- amplifyapp/src/components/doctorForms/DoctorForm.js
+++ amplifyapp/src/components/doctorForms/DoctorForm.js
@@ -1,9 +1,7 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import "./DoctorFormStyles.css";
-
-
-
+import { useSelector } from 'react-redux';
 
 const Forms = () => {
 
