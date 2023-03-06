cac4e328637b9c3e0dfcd887b9c0d5c5cca99f2e
batuhan670 <113347562+batuhan670@users.noreply.github.com>
2023-03-06 17:12:15 +0100

    shieftbanner

--- amplifyapp/src/data/Employee_hours.js
+++ amplifyapp/src/data/Employee_hours.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { SERVER_BASE_URL } from './constants';
 
-const EmployeeHours = () => {
+const EmployeeHours = ({ employeeId }) => {
     const [startTime, setStartTime] = useState(null);
     const [stopTime, setStopTime] = useState(null);
     const [elapsedTime, setElapsedTime] = useState(0);
     const [totalHours, setTotalHours] = useState(0);
     const [isRunning, setIsRunning] = useState(false);
+
+    const employeeID = useSelector((state) => state.user.id);
 
     useEffect(() => {
         if (startTime && stopTime) {
@@ -32,12 +34,17 @@ const EmployeeHours = () => {
     };
 
     const handleSaveClick = () => {
-        axios.post(SERVER_BASE_URL + 'api/employee_hours', { totalHours })
-            .then(res => {
+        axios
+            .post(`${SERVER_BASE_URL}/employees/${employeeId}/hours`, {
+                startTime,
+                stopTime,
+                totalHours,
+            })
+            .then((res) => {
                 console.log(res);
                 console.log(res.data);
             })
-            .catch(error => {
+            .catch((error) => {
                 console.error(error);
             });
     };
@@ -45,15 +52,20 @@ const EmployeeHours = () => {
     return (
         <div>
             <div>
-                <button onClick={handleStartClick} disabled={isRunning}>Start</button>
-                <button onClick={handleStopClick} disabled={!isRunning}>Stop</button>
+                <button onClick={handleStartClick} disabled={isRunning}>
+                    Start
+                </button>
+                <button onClick={handleStopClick} disabled={!isRunning}>
+                    Stop
+                </button>
             </div>
             <div>
-                Elapsed Time: {elapsedTime ? (elapsedTime / (1000 * 60 * 60)).toFixed(2) + ' hours' : 'N/A'}
+                Elapsed Time:{' '}
+                {elapsedTime
+                    ? (elapsedTime / (1000 * 60 * 60)).toFixed(2) + ' hours'
+                    : 'N/A'}
             </div>
-            <div>
-                Total Hours: {totalHours.toFixed(2)} hours
-            </div>
+            <div>Total Hours: {totalHours.toFixed(2)} hours</div>
             <button onClick={handleSaveClick}>Save</button>
         </div>
     );
