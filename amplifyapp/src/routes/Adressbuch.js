bbe7499b49c862015aee1faf45e76c7ab66d53d2
Martin Widhalm <martin.widhalm@gmx.at>
2023-03-07 19:29:50 +0100

    ForceLogin from Main to UsrLgn, started Contacts.

--- amplifyapp/src/routes/Adressbuch.js
+++ amplifyapp/src/routes/Adressbuch.js
@@ -1,14 +1,57 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import React, { useEffect } from 'react'
+import { getEmployeeById } from '../api/api';
+import { useSelector } from 'react-redux';
 import Login from '../logPortal/UserLogin';
 
-const Adressbuch = () => {
-  return (
-    <div>
-      <Login />
-      <div><Link to={`/`}>Back to home</Link></div>
-    </div>
-  )
+let contacts = [];
+function findContacts() {
+  let fedArray = [];
+  for (let index = 0; index < 10; index++) {
+    let employee = getEmployeeById(index);
+    console.log(employee);
+    if ((employee != null) && (employee != undefined)) {
+      fedArray[index] = getEmployeeById(index);
+    };
+  }
+  return fedArray;
+}
+
+function makeContactlist(ContactArray) {
+  let contactList;
+  for (let index = 0; index < ContactArray.length; index++) {
+    contactList += ContactArray[index];
+  }
+  return contactList;
+}
+
+/*
+useEffect(() => {
+  const load = async () => {
+      try {
+          const manager = await getEmployeeById(user.payload.manager_IDemployees);
+          dispatch(setManager(manager))
+      } catch (err) {
+          console.log(err);
+      }
+  }
+  if (!user.payload.manager) {
+      load()
+  }
+}, [user])
+*/
+
+function Adressbuch() {
+  if (useSelector((state) => state.user.user) !== null) {
+    return (
+      <div>
+        <div>Test</div>
+        <div>
+          <ul>{makeContactlist(findContacts(contacts))}</ul></div>
+      </div>
+    );
+  } else {
+    return <Login />;
+  }
 }
 
 export default Adressbuch
--- amplifyapp/src/routes/MainPage.js
+++ amplifyapp/src/routes/MainPage.js
@@ -3,17 +3,10 @@ import Overview from "../components/overview/Overview.js"
 import Shiftshedule from '../components/shiftshedule/Shiftshedule.js';
 import MiscMenues from '../components/miscmenu/MiscMenu.js';
 import Login from "../logPortal/UserLogin.js";
-import { useSelector, useDispatch } from 'react-redux'
-import { setUser } from "../stores/userStore";
+import { useSelector } from 'react-redux'
 
 function MainPage() {
-    const dispatch = useDispatch();
     const user = useSelector((state) => state.user.user);
-    const forcedUser = {
-        id: 3, email: "Mc@Test.net", password: "3",
-        name: "McTest", phone: "0815 4711", department: "IT",
-        position: "Dummy", manager_IDemployees: 1, created_at: "2023-03-03T09:42:45.000Z"
-    }
     if (user != null)
         return (
             <div>
@@ -24,10 +17,7 @@ function MainPage() {
         );
     else
         return (
-            <div>
-                <Login />
-                <button onClick={() => dispatch(setUser(forcedUser))}>Force Login</button>
-            </div>
+            <Login />
         );
 }
 export default MainPage;
