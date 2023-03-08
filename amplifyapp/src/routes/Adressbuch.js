8428579702880b64ecc963498ec0189ce54d5a53
Martin Widhalm <martin.widhalm@gmx.at>
2023-03-08 15:32:32 +0100

    Added AdressbuchStyle, configured inputdata

--- amplifyapp/src/routes/Adressbuch.js
+++ amplifyapp/src/routes/Adressbuch.js
@@ -1,40 +1,48 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { getEmployeeById } from '../api/api';
 import { useSelector } from 'react-redux';
 import Login from '../logPortal/UserLogin';
-
-
+import "./AdressbuchStyle.css";
 
 /*
-useEffect(() => {
-  const load = async () => {
-      try {
-          const manager = await getEmployeeById(user.payload.manager_IDemployees);
-          dispatch(setManager(manager))
-      } catch (err) {
-          console.log(err);
-      }
-  }
-  if (!user.payload.manager) {
-      load()
-  }
-}, [user])
+const express = require('express');
+const router = express.Router()
+const { API_URL } = require("../constants");
+
+//const connection = require('../../data/database/db')
+const ids = () => {
+  const idArray = [];
+  return idArray;
+}
+/*
+const idObject = router.get('/', (res) => {
+  connection.query('SELECT id FROM employees', function (error, results) {
+    if (error) {
+      res.sendStatus(500);
+    } else {
+      res.json(results);
+    }
+  });
+})
 */
 
 function Adressbuch() {
-
+  //console.log(idObject)
   const user = useSelector((state) => state.user.user)
   const [contacts, setContacts] = useState([])
-  const trial = ["Günther", "Klaus", "Kleber"]
   useEffect(() => {
     async function findContacts() {
       let fedArray = [];
-      for (let index = 1; index < 3; index++) {
-        let employee = await getEmployeeById(index);
-        console.log("Employee is " + employee);
-        if ((employee !== null) && (employee !== undefined)) {
-          fedArray.push(employee);
-        };
+      for (let index = 1; index < 30; index++) {
+        try {
+          let employee = await getEmployeeById(index);
+          console.log("Employee is " + employee);
+          if ((employee !== null) && (employee !== undefined)) {
+            fedArray.push(employee);
+          };
+        } catch (error) {
+
+        }
       };
       setContacts(fedArray);
     }
@@ -45,13 +53,21 @@ function Adressbuch() {
 
   function makeContactlist() {
     return (
-      <div>
+      <>
         {contacts.map(
           (element) => {
-            return <li key={element.id}>{element.name}</li>;
+            return (
+              <li className="contact line" key={element.id}>
+                <div>
+                  <div className='contact name'>{element.name}</div>
+                  <div className='contact number'>{element.phone}</div>
+                </div>
+              </li>
+            );
           }
-        )}
-      </div>
+        )
+        }
+      </>
     );
   }
 
