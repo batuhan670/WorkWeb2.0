c911ab69a81ea19ca4da36691427da1c7e4200e3
Martin Widhalm <martin.widhalm@gmx.at>
2023-03-09 15:28:06 +0100

    Configured Newsslide and changed css styles.

--- amplifyapp/src/components/newsslide/Newsslide.js
+++ amplifyapp/src/components/newsslide/Newsslide.js
@@ -3,35 +3,22 @@ import { Carousel } from 'react-responsive-carousel';
 import './NewsslideStyles.css'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-const imgNws = [
-    <div><img height="500px" src="https://i.imgur.com/tLmglQT.png" alt="fakeNews1" />
-        <p className="legend">Legend 1</p></div>,
-    <div><img height="650px" src="https://i.imgur.com/jEm0htO.jpeg" alt="fakeNews2" />
-        <p className="legend">Legend 2</p></div>,
-    <div><img height="700px" src="https://i.imgur.com/JiXkzkL.jpeg" alt="fakeNews3" />
-        <p className="legend">Legend 3</p></div>,
-    <div><img height="750px" src="https://i.imgur.com/KjT7bkl.jpeg" alt="fakeNews4" />
-        <p className="legend">Legend 4</p></div>,
-];
-
 const ImgSrcs = [
-    "https://i.imgur.com/tLmglQT.png",
-    "https://i.imgur.com/jEm0htO.jpeg",
+    "https://thumbs.dreamstime.com/z/businessman-having-good-news-office-47627698.jpg",
+    "https://thumbs.dreamstime.com/z/excited-indian-woman-read-good-news-letter-young-work-computer-home-office-triumph-results-paperwork-correspondence-221752078.jpg",
     "https://i.imgur.com/JiXkzkL.jpeg",
-    "https://i.imgur.com/KjT7bkl.jpeg",
 ]
 
 const ImgDesc = [
-    "Girl chokes on fidget spinner",
-    "Not just life, finds a way",
+    "Webwork enables quick overview on your Workday",
+    "And makes people happy",
     "Dokumentation",
-    "Flashbang struggles"
 ]
 
 function Nwsimg(imagesrc, description) {
     return (
         <div id={imagesrc}>
-            <img className='sldImg' src={imagesrc} alt={description} />
+            <img className='sldImg' src={imagesrc} alt={description} height="650px" />
             <p>{description}</p>
         </div>
     )
@@ -47,7 +34,6 @@ class Newsslide extends Component {
                     {Nwsimg(ImgSrcs[0], ImgDesc[0])}
                     {Nwsimg(ImgSrcs[1], ImgDesc[1])}
                     {Nwsimg(ImgSrcs[2], ImgDesc[2])}
-                    {Nwsimg(ImgSrcs[3], ImgDesc[3])}
                 </Carousel>
             </div>
         );
